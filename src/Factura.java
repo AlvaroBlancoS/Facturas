9e768c82c12db55d75a887785cd5cfd79b4a8496
ablanco <alvaroblanconsangines.to@gmail.com>
2018-10-02 20:35:31 +0200

    Como hacer una factura con IVA

---
+++ src/Factura.java
@@ -0,0 +1,35 @@
+
+public class Factura {
+
+	public static void main(String[] args) {
+		// Declaración de constante
+		final double IVA = 1.19;
+		// Variables locales
+		int importe1 = 25;
+		int importe2 = 1500;
+		int importe3 = 2000;
+		int importe4 = 2500;
+		int importe5 = 3000;
+		int importe6 = 3500;
+		int importe7 = 4000;
+		int importe8 = 4500;
+		int importe9 = 5500;
+		int importe10 = 8000;
+		int importe11 = 10000;
+		
+		
+		System.out.println("El total de la factura1 es " + (importe1 * IVA));
+		System.out.println("El total de la factura2 es " + (importe2 * IVA));
+		System.out.println("El total de la factura3 es " + (importe3 * IVA));
+		System.out.println("El total de la factura4 es " + (importe4 * IVA));
+		System.out.println("El total de la factura5 es " + (importe5 * IVA));
+		System.out.println("El total de la factura6 es " + (importe6 * IVA));
+		System.out.println("El total de la factura7 es " + (importe7 * IVA));
+		System.out.println("El total de la factura8 es " + (importe8 * IVA));
+		System.out.println("El total de la factura9 es " + (importe9 * IVA));
+		System.out.println("El total de la factura10 es " + (importe10 * IVA));
+		System.out.println("El total de la factura11 es " + (importe11 * IVA));
+
+	}
+
+}
